c894612ee4582513087d5de76c587eb729f2b419
Rick South <rick@natamus.com>
2023-12-11 23:45:14 +0100

    Welcome Message source code for 1.20.3.

---
+++ NeoForge/src/main/java/com/natamus/welcomemessage/neoforge/events/NeoForgeWorldJoinEvent.java
@@ -0,0 +1,16 @@
+package com.natamus.welcomemessage.neoforge.events;
+
+import com.natamus.welcomemessage.events.WorldJoinEvent;
+import net.minecraft.world.entity.player.Player;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeWorldJoinEvent {
+	@SubscribeEvent()
+	public static void onSpawn(PlayerEvent.PlayerLoggedInEvent e) {
+		Player player = e.getEntity();
+		WorldJoinEvent.onSpawn(player.level(), player);
+	}
+}
