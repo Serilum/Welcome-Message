c820e42c7c9f2058400665a6cefd7a381ad1e16a
Rick South <rick@natamus.com>
2023-12-11 23:44:51 +0100

    Welcome Message source code for 1.19.4.

---
+++ Forge/src/main/java/com/natamus/welcomemessage/ModForge.java
@@ -0,0 +1,36 @@
+package com.natamus.welcomemessage;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.welcomemessage.forge.config.IntegrateForgeConfig;
+import com.natamus.welcomemessage.forge.events.ForgeWorldJoinEvent;
+import com.natamus.welcomemessage.util.Reference;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Reference.MOD_ID)
+public class ModForge {
+	
+	public ModForge() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		IntegrateForgeConfig.registerScreen(ModLoadingContext.get());
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+    	MinecraftForge.EVENT_BUS.register(new ForgeWorldJoinEvent());
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
