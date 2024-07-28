0aba13dabee2b27c5be5c291f811cb981b8ed869
Rick South <rick@natamus.com>
2024-07-28 07:09:25 +0200

    (v2.6) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Forge/src/main/java/com/natamus/welcomemessage/ModForge.java
+++ Forge/src/main/java/com/natamus/welcomemessage/ModForge.java
@@ -1,6 +1,7 @@
 package com.natamus.welcomemessage;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.welcomemessage.forge.config.IntegrateForgeConfig;
 import com.natamus.welcomemessage.forge.events.ForgeWorldJoinEvent;
 import com.natamus.welcomemessage.util.Reference;
@@ -15,6 +16,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModForge {
 	
 	public ModForge() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		modEventBus.addListener(this::loadComplete);
 
