bf7f796408d1c5f8db0731000225a96868c052a6
Rick South <rick@natamus.com>
2024-07-28 07:09:32 +0200

    (v2.6) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/welcomemessage/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/welcomemessage/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.welcomemessage;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.welcomemessage.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.welcomemessage.neoforge.events.NeoForgeWorldJoinEvent;
 import com.natamus.welcomemessage.util.Reference;
@@ -14,6 +15,10 @@ import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
