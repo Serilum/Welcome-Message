b2f70ebc3b7ba59e7bf97df4f462bd7eacf43fdb
Rick South <rick@natamus.com>
2025-02-12 23:07:41 +0100

    (v2.7) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/welcomemessage/ModForge.java
+++ Forge/src/main/java/com/natamus/welcomemessage/ModForge.java
@@ -32,7 +32,7 @@ public class ModForge {
 	}
 
 	private void loadComplete(final FMLLoadCompleteEvent event) {
-    	MinecraftForge.EVENT_BUS.register(new ForgeWorldJoinEvent());
+    	MinecraftForge.EVENT_BUS.register(ForgeWorldJoinEvent.class);
 	}
 
 	private static void setGlobalConstants() {
--- NeoForge/src/main/java/com/natamus/welcomemessage/neoforge/events/NeoForgeWorldJoinEvent.java
+++ NeoForge/src/main/java/com/natamus/welcomemessage/neoforge/events/NeoForgeWorldJoinEvent.java
@@ -4,9 +4,7 @@ import com.natamus.welcomemessage.events.WorldJoinEvent;
 import net.minecraft.world.entity.player.Player;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 
-@EventBusSubscriber
 public class NeoForgeWorldJoinEvent {
 	@SubscribeEvent()
 	public static void onSpawn(PlayerEvent.PlayerLoggedInEvent e) {
