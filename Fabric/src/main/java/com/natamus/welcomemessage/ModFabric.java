0ce9f9dcfd9f1a4a759d669d69ff7f86107e878f
Rick South <rick@natamus.com>
2025-07-18 02:36:54 +0200

    Welcome Message source code for 1.21.8.

---
+++ Fabric/src/main/java/com/natamus/welcomemessage/ModFabric.java
@@ -0,0 +1,37 @@
+package com.natamus.welcomemessage;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.collective.fabric.callbacks.CollectivePlayerEvents;
+import com.natamus.welcomemessage.events.WorldJoinEvent;
+import com.natamus.welcomemessage.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		CollectivePlayerEvents.PLAYER_LOGGED_IN.register((Level world, Player player) -> {
+			WorldJoinEvent.onSpawn(world, player);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
